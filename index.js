74e0029f8aca38e02a55c866886d84beb291a516
VicWrest <cyganov0206@gmail.com>
2024-10-18 21:26:14 +1000

    with cors local

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const TOKEN = process.env.TOKEN_BOT;
 const app = new express();
 
 app.use(fileUpload({}));
-app.use(cors());
+app.use(cors(
+    {
+        origin:'http://localhost', 
+        credentials:true,     
+        optionSuccessStatus:200
+    }
+));
 app.use(bodyParser.urlencoded({extended: false}));	
 app.use(express.json());
 app.use(bodyParser.json());
@@ -26,11 +32,7 @@ app.get('/', (req, res) => {
 	res.send('<h1>Node application</h1>')
 });
 
-// {
-//     origin:'http://homestorecackes.netlify.app', 
-//     credentials:true,     
-//     optionSuccessStatus:200
-// }
+
 app.use('/api', router);
 
 app.use(errorHandler);
