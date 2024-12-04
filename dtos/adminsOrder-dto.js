0d188ad5eb46f2a3c7e4b80986f8461638940d50
VicWrest <cyganov0206@gmail.com>
2024-12-04 12:18:03 +1000

    with sendOrderAdmin26

--- dtos/adminsOrder-dto.js
+++ dtos/adminsOrder-dto.js
@@ -29,11 +29,13 @@ module.exports = class Dto {
     get messageForAdmin(){
         let mainMessage = 
             `номер заказа: ${this.orderId}
-дата и время создания заказа: ${this.order_date}
+дата и время создания заказа:
+${this.order_date}
 сумма заказа: ${this.summa}
 имя пользователя: @${this.user_name}
 телефон для связи: ${this.phone}
-необходимо выполнить к: ${this.due_date}
+необходимо выполнить к: 
+${this.due_date}
 сумма заказа: ${this.summa}
 
 `
